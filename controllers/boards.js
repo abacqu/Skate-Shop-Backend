5bd249648b4e0a4b441cd10a66581caf704fd02d
alec <abacqu0716@gmail.com>
2022-06-17 12:11:05 -0500

    wip: updates json seed

--- controllers/boards.js
+++ controllers/boards.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const Board = require('../models/board');
 const finishedBoards = require('../boards.json');
+const trucks = require('../trucks');
+const wheels = require('../wheels');
+const bearings = require('../bearings');
 
 const router = express.Router();
 
@@ -16,12 +19,23 @@ router.get('/', (req, res) => {
 })
 
 
-router.get("/finishedboards", (req, res) => {
-    // send projects via JSON
-    console.log('???');
+router.get("/presets", (req, res) => {
     res.json(finishedBoards);
 });
 
+router.put("/modify", (req, res) => {
+    // console.log(finishedBoards, trucks, bearings, wheels);
+    index = wheels.some(w => w.id === req.body.wheelid);
+    trucks.id = req.body.truckid;
+    bearings.id = req.body.bearingid;
+    finishedBoards.name = req.body.boardname;
+    finishedBoards.width = req.body.width;
+
+    // wheel_img = wheels;
+
+    console.log(index);
+    res.json(req.body);
+});
 
 
 module.exports = router;
