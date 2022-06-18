37d2e0b7ae8f2e06cf8c9140702deb1e07fead0a
Cristian Nambo <nambocristian10@gmail.com>
2022-06-17 22:03:18 -0500

    wip: updates mongodb

--- models/bearing.js
+++ models/bearing.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const bearingSchema = new mongoose.Schema({
-    id: {type: String},
     name: {type: String},
     img: {type: String},
     price: {type: Number}
--- wheels.js
+++ wheels.js
@@ -1,30 +1,25 @@
 wheels = [
     {
-        "id": "w01",
         "name": "wheel1",
         "image": "https://cdn.shopify.com/s/files/1/0519/1388/3831/products/ccs-skateboard-wheels-52mm-white_small.jpg?v=1614999048",
         "price": 15
     },
     {
-        "id": "w02",
         "name": "wheel2",
         "image": "https://cdn.shopify.com/s/files/1/0519/1388/3831/products/spitfire-bighead-skateboard-wheels-52mm-1_small.jpg?v=1615010670",
         "price": 14
     },
     {
-        "id": "w03",
         "name": "wheel3",
         "image": "https://cdn.shopify.com/s/files/1/0519/1388/3831/products/888560273900-1_small.jpg?v=1654901543",
         "price": 20
     },
     {
-        "id": "w04",
         "name": "wheel4",
         "image": "https://cdn.shopify.com/s/files/1/0519/1388/3831/products/888560166004-1_small.jpg?v=1615010754",
         "price": 17
     },
     {
-        "id": "w05",
         "name": "wheel5",
         "image": "https://cdn.shopify.com/s/files/1/0519/1388/3831/products/BQ05DD3-1_small.jpg?v=1621499142",
         "price": 16
