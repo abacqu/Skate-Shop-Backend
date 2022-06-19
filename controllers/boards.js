5fe2d534f5c01c1c4d4f4cbad98d77040cf12b16
alec <abacqu0716@gmail.com>
2022-06-19 11:18:46 -0500

    wip: adds create with wip cart and checkout

--- controllers/boards.js
+++ controllers/boards.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const Board = require('../models/board');
 const finishedBoards = require('../boards.json');
-const trucks = require('../models/truck');
-const wheels = require('../models/wheel');
-const bearings = require('../models/bearing');
+const Trucks = require('../models/truck');
+const Wheels = require('../models/wheel');
+const Bearings = require('../models/bearing');
 const Build = require('../models/build');
 
 const router = express.Router();
@@ -34,13 +34,54 @@ router.get("/all", async (req, res) => {
     }
 });
 
-// router.post("/all", async (req, res) => {
-//     try {
-//         res.json(await Board.create(req.body));
-//     } catch (error) {
-//         res.status(400).json(error);
-//     }
-// });
+router.get("/create", async (req, res) => {
+    try {
+        const boards = await Board.find({});
+        const trucks = await Trucks.find({});
+        const wheels = await Wheels.find({});
+        const bearings = await Bearings.find({});
+        res.json(boards + trucks + wheels + bearings); 
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+router.post('/create', async (req, res) => {
+    try{
+        const boardChoice = await Board.findById(req.body.boardId);
+        const boardWidth = boardChoice.width[req.body.boardWidth];
+        const truckChoice = await Trucks.findById(req.body.truckId);
+        const wheelChoice = await Wheels.findById(req.body.wheelId);
+        const bearChoice = await Bearings.findById(req.body.bearingId);
+        res.json(boardChoice + boardWidth + truckChoice + wheelChoice + bearChoice);
+    }
+    catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+router.post('/cart', async (req, res) => {
+    const boardId = req.body.boardId;
+    const wheelId = req.body.wheelId;
+    const truckId = req.body.truckId;
+    const bearId = req.body.bearingId;
+
+
+});
+
+router.post('/checkout', async (req, res) => {
+    const boardId = req.body.boardId;
+    const wheelId = req.body.wheelId;
+    const truckId = req.body.truckId;
+    const bearId = req.body.bearingId;
+
+    const newOrder = new Order({board: boardId, wheel: wheelId, truck: truckId, bearing: bearId});
+
+    newOrder.save(function(err) {
+        if(err) return handleError(err);
+    });
+});
+
 
 // Delete
 
