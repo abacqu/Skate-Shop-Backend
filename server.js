d267a6c387c40059cfd502fb3117037d687904d4
Peter Yoo <102557183+PeterSYoo@users.noreply.github.com>
2022-06-23 19:15:41 -0700

    Revert "wip: edits firebase"

--- server.js
+++ server.js
@@ -6,13 +6,6 @@ const morgan = require('morgan');
 const cors = require('cors');
 const cartController = require('./controllers/carts');
 const boardController = require('./controllers/boards');
-// const admin = require('firebase-admin');
-
-// const serviceAccount = require('./firebase-service-key.json');
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
 
 // Config App settings
 require('dotenv').config();
@@ -20,12 +13,12 @@ require('dotenv').config();
 const { PORT, MONGODB_URL } = process.env;
 
 // Connect to MONGODB
-mongoose.connect(MONGODB_URL);
+mongoose.connect(MONGODB_URL)
 
 // MONGODB status
 mongoose.connection
-.on('connected', () => console.log('Connected to MongoDB'));
-.on('error', (err) => console.log('Error with MongoDB' + err.message));
+.on('connected', () => console.log('Connected to MongoDB'))
+.on('error', (err) => console.log('Error with MongoDB' + err.message))
 
 // Middleware
 app.use(cors());
@@ -34,17 +27,7 @@ app.use(express.json());
 app.use('/', boardController);
 app.use('/', cartController);
 
-// // Authorization Middleware
-// app.use(async (req, res, next) => {
-//   const token = req.get('Authorization');
-//   if(token) {
-//     const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
-//     console.log(user);
-//   }
-
-//   next();
-// });
 
 
 // Listener
-app.listen(PORT, () => console.log(`Express is listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Express is listening on port ${PORT}`))
